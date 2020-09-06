7f890c1b9e0b376fd42c6655023df79f87472617
nelsonjimenez1 <53049446+nelsonjimenez1@users.noreply.github.com>
2020-09-05 20:28:38 -0500

    se agrego la actividad de registrar calendario

---
+++ Schedulock/app/src/main/java/com/lazyswift/schedulock/registrar_calendario.java
@@ -0,0 +1,14 @@
+package com.lazyswift.schedulock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrar_calendario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_calendario);
+    }
+}
