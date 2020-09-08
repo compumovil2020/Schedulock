6246e49f8bdd1f8cf82a0bfa718d6ee54bf75f02
Andres G <andrew.2904@outlook.com>
2020-09-08 08:50:06 -0500

    Se sube el menu principal

--- Schedulock/app/src/main/java/com/lazyswift/schedulock/Login.java
+++ Schedulock/app/src/main/java/com/lazyswift/schedulock/Login.java
@@ -2,13 +2,26 @@ package com.lazyswift.schedulock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Login extends AppCompatActivity {
+
+    private ImageButton btnIniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.iniciar_sesion_layout);
+        btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent actividadMenuPrincipal = new Intent(view.getContext(), MenuPrincipalActivity.class);
+                startActivity(actividadMenuPrincipal);
+            }
+        });
     }
 }
