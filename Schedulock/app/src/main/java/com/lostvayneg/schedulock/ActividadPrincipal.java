41780b864ced41dfa1ff9d898afa8b3fffaec08c
SandraIsabelChavezAlcalde <wandasan10@hotmail.com>
2020-10-20 17:30:56 -0500

    Primera version de autenticacion

--- Schedulock/app/src/main/java/com/lostvayneg/schedulock/ActividadPrincipal.java
+++ Schedulock/app/src/main/java/com/lostvayneg/schedulock/ActividadPrincipal.java
@@ -1,8 +1,19 @@
 package com.lostvayneg.schedulock;
 
+import android.content.Intent;
 import android.os.Bundle;
-import com.google.android.material.navigation.NavigationView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.lostvayneg.schedulock.Controladores_de_Eventos.Login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -11,22 +22,27 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class ActividadPrincipal extends AppCompatActivity {
+public class ActividadPrincipal extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private AppBarConfiguration listaFragmentos;
+    private FirebaseAuth mAuth;
+    private NavigationView vistaNavegacion;
+    private NavController controladorNavegacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actividad_principal);
         //Inflacion de elementos
+        mAuth = FirebaseAuth.getInstance();
         //Barra superior de la app
         Toolbar barraNavegacion = findViewById(R.id.barra_navegacion);
         setSupportActionBar(barraNavegacion);
         //Menu lateral
         DrawerLayout menuLateral = findViewById(R.id.menu_lateral);
         //Panel del lateral
-        NavigationView vistaNavegacion = findViewById(R.id.vista_navegacion);
+        vistaNavegacion = findViewById(R.id.vista_navegacion);
+        vistaNavegacion.setNavigationItemSelectedListener(this);
         listaFragmentos = new AppBarConfiguration.Builder(
                 R.id.frg_ver_perfil,
                 R.id.frg_menu_principal,
@@ -35,7 +51,7 @@ public class ActividadPrincipal extends AppCompatActivity {
                 R.id.frg_ver_logros)
                 .setDrawerLayout(menuLateral)
                 .build();
-        NavController controladorNavegacion = Navigation.findNavController(this, R.id.vista_fragmentos);
+        controladorNavegacion = Navigation.findNavController(this, R.id.vista_fragmentos);
         NavigationUI.setupActionBarWithNavController(this, controladorNavegacion, listaFragmentos);
         NavigationUI.setupWithNavController(vistaNavegacion, controladorNavegacion);
     }
@@ -45,5 +61,19 @@ public class ActividadPrincipal extends AppCompatActivity {
         NavController controladorNavegacion = Navigation.findNavController(this, R.id.vista_fragmentos);
         return NavigationUI.navigateUp(controladorNavegacion, listaFragmentos)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.frg_cerrar_sesion:
+                Log.i("SDSDF","SDASA");
+                Toast.makeText(ActividadPrincipal.this,"Funciona",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.frg_ver_calendarios:
+                controladorNavegacion.navigate(R.id.ir_de_menu_principal_a_ver_logros);
+                return true;
+        }
+        return true;
     }
 }
--- Schedulock/app/src/main/java/com/lostvayneg/schedulock/Controladores_de_Eventos/Login.java
+++ Schedulock/app/src/main/java/com/lostvayneg/schedulock/Controladores_de_Eventos/Login.java
@@ -3,27 +3,95 @@ package com.lostvayneg.schedulock.Controladores_de_Eventos;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.lostvayneg.schedulock.ActividadPrincipal;
 import com.lostvayneg.schedulock.R;
 
 public class Login extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private  EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actividad_iniciar_sesion);
+        mAuth = FirebaseAuth.getInstance();
+        email= findViewById(R.id.email_user_iniciar_sesion);
+        password= findViewById(R.id.password_user_iniciar_sesion);
     }
 
     public void onClickiniciarSesion(View v){
-        Intent actividadMenuPrincipal = new Intent(this, ActividadPrincipal.class);
-        startActivity(actividadMenuPrincipal);
+        String emailin;
+        String passwordin;
+        emailin=email.getText().toString();
+        passwordin=password.getText().toString();
+        if(!emailin.isEmpty() && isEmailValid(emailin)){
+            if(!passwordin.isEmpty()){
+                mAuth.signInWithEmailAndPassword(emailin, passwordin)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Login.this, "Usuario o contraseña inválidos.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+
+                                // ...
+                            }
+                        });
+            }
+            else{
+                Toast.makeText(Login.this,"Ingrese una contraseña",Toast.LENGTH_LONG).show();
+            }
+        }
+        else
+        {
+            Toast.makeText(Login.this,"Ingrese un correo",Toast.LENGTH_LONG).show();
+        }
     }
 
     public void onClickRegistrarse(View v){
         Intent actividadRegistroUsuario = new Intent(this, RegistrarUsuario.class);
         startActivity(actividadRegistroUsuario);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    private void updateUI(FirebaseUser currentUser){
+        if(currentUser!=null){
+            Intent intent = new Intent(getBaseContext(), ActividadPrincipal.class);
+            intent.putExtra("user", currentUser.getEmail());
+            startActivity(intent);
+        } else {
+            email.setText("");
+            password.setText("");
+        }
+    }
+    private boolean isEmailValid(String email) {
+        if (!email.contains("@") ||
+                !email.contains(".") ||
+                email.length() < 5)
+            return false;
+        return true;
+    }
+
 }
