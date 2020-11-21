3240d8ac4c97b76d818620f0d076caf0dc5c8221
Andres G <andrew.2904@outlook.com>
2020-11-21 15:08:57 -0500

    Cambios visuales en ver perfil

--- Schedulock/app/src/main/java/com/lostvayneg/schedulock/Controladores_de_Eventos/FragmentoVerPerfil.java
+++ Schedulock/app/src/main/java/com/lostvayneg/schedulock/Controladores_de_Eventos/FragmentoVerPerfil.java
@@ -9,13 +9,25 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.lostvayneg.schedulock.Entidades.Actividad;
+import com.lostvayneg.schedulock.Entidades.Calendario;
 import com.lostvayneg.schedulock.Entidades.Usuario;
 import com.lostvayneg.schedulock.Enumerados.Enum_Niveles;
 import com.lostvayneg.schedulock.R;
@@ -23,6 +35,8 @@ import com.lostvayneg.schedulock.Utilidades.Acceso_Base_Datos;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FragmentoVerPerfil extends Fragment {
@@ -44,8 +58,20 @@ public class FragmentoVerPerfil extends Fragment {
     private EditText editar_edad_usuario;
     private TextView genero_usuario;
     private Button guardar_cambios;
+    private LinearLayout ly_logros;
+    private LinearLayout ly_calendarios;
+    private LinearLayout ly_notas;
+
+    private NavController controladorNavegacion;
 
     private Usuario usuario_actual;
+    public static final String RUTA_ACTIVIDADES ="actividades/";
+    public static final String RUTA_CALENDARIOS ="calendarios/";
+    public static final String RUTA_LOGROS ="logros/";
+    public static final String RUTA_NOTAS ="notas/";
+    public static final String RUTA_USUARIOS ="usuarios/";
+    public DatabaseReference referenciaBD;
+    public FirebaseDatabase baseDatos;
 
     @Nullable
     @Override
@@ -69,8 +95,43 @@ public class FragmentoVerPerfil extends Fragment {
         editar_edad_usuario = pantalla.findViewById(R.id.entrada_edad_usuario_ver_perfil);
         genero_usuario = pantalla.findViewById(R.id.genero_usuario_ver_perfil);
         guardar_cambios = pantalla.findViewById(R.id.btn_guardar_cambios_ver_perfil);
+        ly_logros = pantalla.findViewById(R.id.ly_logros_ver_perfil);
+        ly_calendarios = pantalla.findViewById(R.id.ly_calendarios_ver_perfil);
+        ly_notas = pantalla.findViewById(R.id.ly_notas_ver_perfil);
 
+        controladorNavegacion = Navigation.findNavController(getActivity(), R.id.vista_fragmentos);
+
+        baseDatos = FirebaseDatabase.getInstance();
         cargarDatosIniciales();
+        agregarListeners();
+
+        guardar_cambios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guardarCambios();
+            }
+        });
+
+        ly_logros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                controladorNavegacion.navigate(R.id.frg_ver_logros);
+            }
+        });
+
+        ly_calendarios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                controladorNavegacion.navigate(R.id.frg_ver_calendarios);
+            }
+        });
+
+        ly_notas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                controladorNavegacion.navigate(R.id.frg_ver_notas);
+            }
+        });
 
         return pantalla;
     }
@@ -89,6 +150,10 @@ public class FragmentoVerPerfil extends Fragment {
                 editar_correo_usuario.setText(usuario_actual.getEmail());
                 editar_edad_usuario.setText(usuario_actual.getEdad());
                 genero_usuario.setText(usuario_actual.getGenero());
+                cargarCantidadLogros();
+                cargarCantidadCalendarios();
+                cargarCantidadNotas();
+                cargarCantidadActividades();
             }
 
             File img_usuario = (File) datosIniciales.getSerializable("file_imagen_usuario");
@@ -103,14 +168,104 @@ public class FragmentoVerPerfil extends Fragment {
         Enum_Niveles enum_niveles = null;
 
         String expRestante;
+        int progreso;
         if(usuario_actual.getNivel() == 0){
             enum_niveles = Enum_Niveles.NIVEL_1;
             expRestante = usuario_actual.getExperiencia() + "/" + enum_niveles.getPuntosNecesarios() + " Puntos";
+            progreso = (int)(usuario_actual.getExperiencia() * 100) / enum_niveles.getPuntosNecesarios();
+            progeso_exp.setProgress(progreso);
         }
         else{
             enum_niveles = enum_niveles.obtenerNivel(usuario_actual.getNivel());
             expRestante = usuario_actual.getExperiencia() + "/" + enum_niveles.getPuntosNecesarios() + " Puntos";
+            progreso = (int)(usuario_actual.getExperiencia() * 100) / enum_niveles.getPuntosNecesarios();
+            progeso_exp.setProgress(progreso);
         }
         return expRestante;
     }
+
+    private void cargarCantidadNotas(){
+        referenciaBD = baseDatos.getReference(RUTA_NOTAS + FirebaseAuth.getInstance().getCurrentUser().getUid() + "/");
+        referenciaBD.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                cantidad_notas.setText(""+dataSnapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void cargarCantidadLogros(){
+        referenciaBD = baseDatos.getReference(RUTA_LOGROS + FirebaseAuth.getInstance().getCurrentUser().getUid() + "/");
+        referenciaBD.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                cantidad_logros.setText(""+dataSnapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void cargarCantidadCalendarios(){
+        referenciaBD = baseDatos.getReference(RUTA_CALENDARIOS + FirebaseAuth.getInstance().getCurrentUser().getUid() + "/");
+        referenciaBD.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                cantidad_calendarios.setText(""+dataSnapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void cargarCantidadActividades(){
+        referenciaBD = baseDatos.getReference(RUTA_ACTIVIDADES + FirebaseAuth.getInstance().getCurrentUser().getUid() + "/");
+        referenciaBD.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                cantidad_actividades.setText(""+dataSnapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void agregarListeners(){
+
+    }
+
+    private void guardarCambios(){
+        String nombre = editar_nombre_usuario.getText().toString();
+        String edad = editar_edad_usuario.getText().toString();
+        if(!nombre.isEmpty()){
+            if(!edad.isEmpty()){
+                usuario_actual.setEdad(edad);
+                usuario_actual.setNombre(nombre);
+                referenciaBD = baseDatos.getReference(RUTA_USUARIOS + FirebaseAuth.getInstance().getCurrentUser().getUid());
+                referenciaBD.setValue(usuario_actual);
+                nombre_usuario.setText(usuario_actual.getNombre());
+            }
+            else{
+                Toast.makeText(getContext(), "La edad de usuario no debe estar vacio", Toast.LENGTH_LONG).show();
+            }
+        }
+        else{
+            Toast.makeText(getContext(), "El nombre de usuario no debe estar vacio", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
